5a2531bbdbc281efc86a67b71d8aabb8d4cd8d0e
ihruch <ihruch@gmail.com>
2020-08-25 18:12:19 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'users-list';
+
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
---
+++ src/app/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+
+// @ts-ignore
+@NgModule({
+  imports: [
+    MatListModule
+  ],
+  exports: [
+    MatListModule
+  ]
+
+})
+export class MaterialModule {}
